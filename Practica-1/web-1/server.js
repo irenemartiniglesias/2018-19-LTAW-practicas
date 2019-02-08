1762288a1e99f0a6b3190240f03eccc54de4ce09
irenemartiniglesias <i.martini@alumnos.urjc.es>
2019-02-08 15:38:20 +0100

    introduccion de fecha y hora en modulo propio

--- Practica-1/web-1/server.js
+++ Practica-1/web-1/server.js
@@ -1,10 +1,11 @@
 var http = require('http');
+var dt = require('./myfirstmolude');
 
 console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write("The date and time are currently: " + dt.myDateTime());
   console.log("Peticion atendida")
 }).listen(8080);
