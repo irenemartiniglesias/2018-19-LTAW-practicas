2336c7886518fa1086b6a94ea1f477341648c914
irenemartiniglesias <i.martini@alumnos.urjc.es>
2019-03-11 13:46:40 +0100

    Practica 2, con formulario

--- Practica-2/mi_proyectoweb/mi_tienda/urls.py
+++ Practica-2/mi_proyectoweb/mi_tienda/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.home_view),
+    url(r'^list/', views.list),
+    url(r'^moviles/', views.moviles),
+    url(r'^order/', views.order),
+    url(r'^factura/', views.factura),
 ]
--- Practica-2/mi_proyectoweb/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb/mi_tienda/views.py
@@ -3,8 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+
+from mi_tienda.models import Product
 
 # Create your views here.
 def home_view (request):
     return render(request, "index.html", {})
+
+def moviles(request):
+    return render(request, "moviles.html", {})
+
+def order(request):
+    return render(request, "order.html", {})
+
+def factura(request):
+    return render(request, "factura.html", {})
+
+def list(request):
+    objects = Product.objects.all()
+    html = "<p>Listado de articulos</p>"
+    for elt in objects:
+        print(elt.name)
+        html += '<p>'+ elt.name + " stock: " + str(elt.stock)+ " precio: "+ str(elt.price) + '<p>'
+    return HttpResponse(html)
