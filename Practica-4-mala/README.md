ed91f281d62f1bae61ec7aaa4b31e76e960d9bad
irenemartiniglesias <i.martini@alumnos.urjc.es>
2019-04-17 18:08:38 +0200

    .

--- Practica-4-mala/README.md
+++
@@ -1,3 +0,0 @@
-# Práctica 4
-
-En este repo se colocarán los ficheros de la práctica 4 de LTAW
--- Practica-4-mala/chat-clients.js
+++
@@ -1,7 +0,0 @@
-function main() {
-  console.log("Hola!!!!")
-
-  //-- Crear un socket.io. Se establece la conexion
-  //-- con el servidor
-  var socket = io();
-}
